96ad792244c74321e06b31de139b8f6ae38e2b5a
nick <nick@oppen.net.au>
2023-01-16 13:17:14 +1100

    Nw more general (see warning in /grokTestBed/README.md)

--- grokTestBed/Program.cs
+++ grokTestBed/Program.cs
@@ -1,52 +1,141 @@
 ﻿using GrokNet;
+using Microsoft.VisualBasic;
+using System.Collections;
 using System.Diagnostics;
 using System.Globalization;
 
 class Program
 {
-    private int readLogFileToDatabase(FileInfo fileInformation, StreamWriter errorLogWriter, Program p)
+    private struct patternName
+    {
+        public string pattern;
+        public string name;
+        public int index;
+    }
+    private struct cliArgs
+    {
+        public string customPatternFile;
+        public string patternFile;
+        public string dataFile;
+        public string outputFile;
+        public int verbosity;
+    }
+
+    private cliArgs readCliArgs(string[] strArgs)
+    {
+        cliArgs result = new cliArgs
+        {
+            customPatternFile = @"./grokCustom.txt",
+            patternFile= @"./matchPattern.txt",
+            dataFile= @"./grokTest.txt",
+            outputFile = @"./outputFile.txt",
+            verbosity = 2
+        };
+
+        for (int i = 0; i < strArgs.Length; i++)
+        {
+            switch (strArgs[i])
+            {
+                case "-p":
+                    result.customPatternFile = strArgs[++i];
+                    break;
+                case "-f":
+                    result.dataFile = strArgs[++i];
+                    break;
+                case "-c":
+                    result.customPatternFile += strArgs[++i];
+                    break;
+                case "-o":
+                    result.outputFile += strArgs[++i];
+                    break;
+                case "-v0":
+                    result.verbosity = 0;
+                    break;
+                case "-v1":
+                    result.verbosity = 1;
+                    break;
+                case "-v2":
+                    result.verbosity = 2;
+                    break;
+                default:
+                    Console.WriteLine("-f <dataFile> (default: ./grokTest)\n-p <top level pattern file> (default: ./matchPattern.txt)\n-c <custom pattern file> (default: ./grokCustom.txt)\n-o <output file> (default: ./outputFile.txt)\n-vx : Verbosity (0 <= x <= 2)");
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+        return result;
+    }
+    private ArrayList splitPatternIntoElements(string grokPattern)
+    {
+        int i = 0;
+        ArrayList result = new ArrayList();
+        string[] patternColonNames = grokPattern.Split('%');        /// Problem: \% could be part of the string
+        foreach (string patternColonName in patternColonNames)
+        {
+            string[] patternsAndNames = patternColonName.Split(":");    /// Ditto: \:
+            if (patternsAndNames.Length >= 2 )
+                result.Add(new patternName { pattern = patternsAndNames[0].TrimStart('{'), name = patternsAndNames[1].TrimEnd(' ','}'), index = i++ });
+        }
+
+        return result;
+    }
+    private int readLogFileToDatabase(FileInfo fileInformation, StreamWriter errorLogWriter, cliArgs args, Program p)
     {
         int insertCount = 0;
+        var parseTime = new Stopwatch();
 
-        string customPatternFile = @"./grokCustom.txt";
-        if (File.Exists(customPatternFile))
+        if (File.Exists(args.customPatternFile) && File.Exists(args.patternFile))
         {
-            using (FileStream customPatterns = System.IO.File.OpenRead(customPatternFile))
-            {
-                StreamReader logReader = fileInformation.OpenText();
-                var grkLogLine = new Grok("%{LOGTIME:Timestamp} %{LOGPROG:Prog}: %{LOGACTION:Action} %{LOGDOMAIN:Domain} %{LOGDIRECTION:Direction} %{LOGEOL:EndOfLine}", customPatterns);
-                //                                      0                 1                   2                   3                       4                   5                  
-                GrokResult grkLogRes;
-                string timeOfCall, action, domain, direction, endOfLine;
-                int isClauseLength, toPos;
-                string logLine;
+            var grokPattern = File.ReadAllText(args.patternFile);
+            var grokPatternElements = p.splitPatternIntoElements(grokPattern);
+            var customPatterns = System.IO.File.OpenRead(args.customPatternFile);
+            
+            StreamReader logReader = fileInformation.OpenText();
+            var grkLogLine = new Grok(grokPattern, customPatterns); //e.g. "%{LOGTIME:Timestamp} %{LOGPROG:Prog}: %{LOGACTION:Action} %{LOGDOMAIN:Domain} %{LOGDIRECTION:Direction} %{LOGEOL:EndOfLine}"
+                                           //                                           0                 1                   2                   3                       4                   5                  
+            GrokResult grkLogRes;
+            string logLine;
 
-                while ((logLine = logReader.ReadLine()) != null)
+            while ((logLine = logReader.ReadLine()) != null)
+            {
+                parseTime.Start();
+                grkLogRes = grkLogLine.Parse(logLine, 10000000);    // 10000000 is 1 sec in ticks
+                parseTime.Stop();
+
+                if (grkLogRes.Any())
                 {
-                    grkLogRes = grkLogLine.Parse(logLine);
-                    if (grkLogRes.Any())
+                    if (args.verbosity >= 2)
                     {
-                        timeOfCall = grkLogRes[0].Value.ToString();
-                        Console.WriteLine("TimeOfCall:\t" + timeOfCall);
-                        action = grkLogRes[2].Value.ToString();
-                        Console.WriteLine("Action:\t\t" + action );
-                        domain = grkLogRes[3].Value.ToString();
-                        Console.WriteLine("Domain:\t\t" + domain);
-                        direction = grkLogRes[4].Value.ToString();
-                        Console.WriteLine("Direction:\t" + direction);
-                        endOfLine = grkLogRes[5].Value.ToString();
-                        Console.WriteLine("End Of Line:\t" + direction);
+                        Console.WriteLine("Parse time:" + parseTime.Elapsed.ToString() + " --- " + logLine);
+                        foreach (patternName element in grokPatternElements)
+                        {
+                            Console.WriteLine(element.name + ":\t" + grkLogRes[element.index].Value.ToString());
+                        }
+                    }
+                    errorLogWriter.WriteLine("Parse time:" + parseTime.Elapsed.ToString() + " --- " + logLine);
+                    foreach (patternName element in grokPatternElements)
+                    {
+                        errorLogWriter.WriteLine(element.name + ":\t" + grkLogRes[element.index].Value.ToString());
+                    }
+                    errorLogWriter.WriteLine();
+                }
+                else
+                {
+                    errorLogWriter.WriteLine("No Match Error:" + logLine);
+                    if (args.verbosity >= 1)
+                    {
+                        Console.WriteLine("No Match Error:" + logLine);
                         Console.WriteLine();
                     }
-                    else
-                        errorLogWriter.WriteLine("No Match Error:" + logLine);
                 }
-                logReader.Close();
+                parseTime.Reset();
+                insertCount++;
             }
+            logReader.Close();
         }
         else
         {
-            errorLogWriter.WriteLine("File: " + customPatternFile + " does not exist");
+            Console.WriteLine("File: " + args.customPatternFile + " and/or file: " + args.patternFile + " does not exist");
             errorLogWriter.Close();
             Environment.Exit(-1);
         }
@@ -57,20 +146,19 @@ class Program
     static void Main(string[] strArgs)
     {
         var p = new Program();
+        var args = p.readCliArgs(strArgs);
 
-        int insertCount;
-        TimeSpan insertTime = TimeSpan.Zero;
-        var runTimer = new Stopwatch();
+        int insertCount = 0;
 
-        if (File.Exists(strArgs[0]))
+        if (File.Exists(args.dataFile))
         {
-            using (StreamWriter errorLogWriter = new StreamWriter("./errorLog.txt", false))
+            using (var errorLogWriter = new StreamWriter(args.outputFile, false))
             {
-                FileInfo fileInformation = new FileInfo(strArgs[0]);
-                insertCount = p.readLogFileToDatabase(fileInformation, errorLogWriter, p);
+                FileInfo fileInformation = new FileInfo(args.dataFile);
+                insertCount = p.readLogFileToDatabase(fileInformation, errorLogWriter, args, p);
             }
         }
-
+        Console.WriteLine("lines: " + insertCount.ToString());
 
         Environment.Exit(0);
     }
--- grokTestBed/README.md
+++ grokTestBed/README.md
@@ -1,2 +1,14 @@
 ﻿
-The files grokCustom.txt needs to be copied to the executable's directory and the location of grokTestLongEOL.txt needs to be the single command line arguement.
+Switches now control input files and the output destination:
+
+-f <dataFile> (default: ./grokTest)
+-p <top level pattern file> (default: ./matchPattern.txt)
+-c <custom pattern file> (default: ./grokCustom.txt)
+-o <output file> (default: ./outputFile.txt)
+
+Verbosity to the Console can be controlled using the -vx switch:
+-v0: output goes to the output file only
+-v1: all output goes to the output file and only errors to the console
+-v2: all output goes to both output file and console
+
+Warning: If there are \{ \} \: or \% characters in the top level pattern this will screw up the output.
